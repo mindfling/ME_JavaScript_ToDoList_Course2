b798b07d1f0ec7e77f399286c70e383f6c20841f
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-11-13 03:22:38 +0300

    add elements.js file for DOM

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // * main index script *
 
+import app from './modules/elements.js';
 import {trans} from './modules/utils.js';
+
+console.log('app: ', app);
 
 
 const init = () => {
@@ -8,11 +11,12 @@ const init = () => {
   const userNameCyr = prompt('Введите имя пользователя приложением', 'Емпти');
   const latUserName = trans(userNameCyr.trim());
   const STORAGE_KEY = 'todo_app_' + latUserName;
+  
   // console.log('userNameCyr: ', userNameCyr);
   // console.log('latUserName: ', latUserName);
   console.log('STORAGE_KEY: ', STORAGE_KEY);
 
-  
+
 };
 
 window.initTodo = init;
---
+++ js/modules/elements.js
@@ -0,0 +1,4 @@
+// * dom consts app elements:
+const app = document.querySelector('.app-container');
+
+export default {app};
